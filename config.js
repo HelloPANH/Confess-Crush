758fa20d097e2080f8ef63181994548955e4cbc4
HelloPANH <97031931+HelloPANH@users.noreply.github.com>
2022-01-09 09:20:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+const CONFIG = {êuương onhAnghuPNig
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
